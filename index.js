28ff8931f8c28386153ecb1761525abab73d9467
dapod93 <davidliois50@gmail.com>
2025-03-18 18:13:00 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 require("dotenv").config();
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
-const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const app = express();
+const sequelize = new Sequelize({ dialect: "sqlite", storage: "db.sqlite" });
+
+class User extends Model {}
+User.init(
+  {
+    username: DataTypes.STRING,
+  },
+  { sequelize, modelName: "users" }
+);
+User.hasMany(ExerciseLog);
+
+class ExerciseLog extends Model {}
+ExerciseLog.init({
+  user_id: DataTypes.INTEGER,
+  description: DataTypes.STRING,
+  duration: DataTypes.INTEGER,
+  date: DataTypes.TIME,
+});
+ExerciseLog.belongsTo(User, { foreignKey: "user_id" });
+
+sequelize.sync();
 
 app.use(cors());
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
