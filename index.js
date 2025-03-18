35fde57038a8262f9c2cd0f0819fc5eedd4bdd27
dapod93 <davidliois50@gmail.com>
2025-03-18 21:24:31 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- index.js
+++ index.js
@@ -112,9 +112,9 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
   }
 
   // To prevent database spamming, since using free deployment platform
-  // if ((await ExerciseLog.count({ where: { user_id: user.id } })) >= 5) {
-  //   return res.json({ error: "limit reached" });
-  // }
+  if ((await ExerciseLog.count({ where: { user_id: user.id } })) >= 5) {
+    return res.json({ error: "limit reached" });
+  }
 
   if (req.body.description === null || req.body.description === "") {
     return res.json({ error: "description is empty" });
